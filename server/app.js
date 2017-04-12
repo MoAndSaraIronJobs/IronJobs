91f1f534ff9d71c5b669f6193c36c045711ff96f
Sara Basile <sarabasile@Saras-Air.lan>
2017-04-12 14:35:48 -0400

    required express & body parser, set listen port & log message

--- server/app.js
+++ server/app.js
@@ -1,2 +1,13 @@
 
-console.log('Hello World');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+let app = express();
+
+app.use(bodyParser.json());
+
+
+
+app.listen(3000, function startServer() {
+  console.log('Server is up!');
+});
