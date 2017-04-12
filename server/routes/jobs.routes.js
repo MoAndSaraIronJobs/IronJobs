57dfc3f9010ec13c477ad85091685a1a5f1a6f44
Sara Basile <sarabasile@Saras-Air.lan>
2017-04-12 15:29:34 -0400

    made fake data - object in an array

--- server/routes/jobs.routes.js
+++ server/routes/jobs.routes.js
@@ -1,4 +1,19 @@
 const jobRouter = require('express').Router();
 
+let allJobs = [
+  {
+    "id": "12345",
+    "company": "Etsy",
+    "link": "www.etsy.com",
+    "notes": "awesome",
+    "createTime": "12:34"
+  }
+];
+
+jobRouter.get('/', function functionName() {
+
+});
+
+
 
 module.exports = jobRouter;
