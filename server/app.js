d72f00345829909549170d12b8d2a94c1df9a2f6
Sara Basile <sarabasile@Saras-Air.lan>
2017-04-13 14:28:04 -0400

    database works

--- server/app.js
+++ server/app.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+require('./database-setup.js');
+
 let app = express();
 
 app.use(bodyParser.json());
--- server/database-setup.js
+++ server/database-setup.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 
-if(!process.env.MY_DB_LOCATION) {
+console.log('in db setup', process.env.MONGODB_URI);
+
+if(!process.env.MONGODB_URI) {
   console.error('No database detected!');
-  process.exit(1337);
+  process.exit(128);
 }
 
-mongoose.connect(process.env.DB_LOCATION);
+mongoose.connect(process.env.MONGODB_URI);
