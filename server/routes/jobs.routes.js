1155eeb6da71c6f9347db9b0874f89cfb5418b47
Sara Basile <sarabasile@Saras-Air.lan>
2017-04-13 16:32:34 -0400

    post new job works. get single job in progress

--- server/routes/jobs.routes.js
+++ server/routes/jobs.routes.js
@@ -9,7 +9,6 @@ const Job = require('../models/Job.model.js');
 * @return {void}
 */
 jobRouter.get('/', function getAllJobs(req, res, next) {
-
   Job.find()
   .then(function sendAllJobs(allJobs) {
     if(!Array.isArray(allJobs)) {
@@ -17,7 +16,6 @@ jobRouter.get('/', function getAllJobs(req, res, next) {
       err.status = 500;
       return next();
     }
-
     res.json(allJobs.map(function(obj) {
       return {
         id: obj.id,
@@ -29,18 +27,43 @@ jobRouter.get('/', function getAllJobs(req, res, next) {
   .catch(function handleErrors(err) {
     let theError = new Error('Could not get all jobs');
     theError.status = 422;
+    return next(theError);
   });
+});
+
+jobRouter.get('/:id', function getSingleJob(req, res, next) {
+  Job.findById(req.params.id)
+    .then(function sendSingleJob(job) {
+      res.json(allJobs[req.params.id]);
+    })
+    .catch(function handleErrors(err) {
+      let theError = new Error('Could not find this job');
+      theError.status = 404;
+      return next(theError);
+    });
 });
 
 jobRouter.post('/', function postNewJob(req, res, next) {
   // TODO: add data audits for lack of required fields
   console.log(req.body);
-  res.json({
+
+  let newJob = new Job( {
     company: 'google',
     link: 'www.google.com',
     notes: 'more awesome',
     createTime: Date.now()
   });
+
+  newJob.save()
+    .then(function sendResponse(data) {
+      res.json({message: 'I have no idea what to put here!'});
+    })
+    .catch(function handleErrors(err) {
+      let theError = new Error('Could not post a new job');
+     theError.status = 422;
+     return next(theError);
+    });
+
 });
 
 
